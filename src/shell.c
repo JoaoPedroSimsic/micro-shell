e1694b7ffe32451303302c4c74157ceee6e8ee56
JoaoPedroSimsic <joao.simsic@4x4brasil.com.br>
2025-08-19 22:32:54 -0300

    fix: shell files

--- src/shell.c
+++ src/shell.c
@@ -1,26 +1,11 @@
 #include <stdio.h>
-#include <string.h>
-// #include "parser.h"
-// #include "builtins.h"
-// #include "exec.h"
 
-#define INPUT_SIZE 1024
+int shell_loop() {
 
-void shell_loop() {
-	char input[INPUT_SIZE];
-	char *argv[64];
-
-	while (1) {
-		printf("-> ");
+	while(1) {
+		printf("shell$>");
 		fflush(stdout);
 
-		if (!fgets(input, INPUT_SIZE, stdin)) {
-			printf("\n");
-			break;
-		}
-
-		input[strcspn(input, "\n")] = 0;
-
-		int argc = parse_input(input, argv);
+		return 0;
 	}
 }
